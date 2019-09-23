08498dc045e5f570b6ac7f34d2e4c1a2adce8520
adizbek <adizbek1998@gmail.com>
2019-09-23 23:08:11 +0500

    Code structure improved & Integrated AndroidX

---
+++ src/main/java/com/github/adizbek/sper/ui/RVEmptyObserver.java
@@ -0,0 +1,61 @@
+package com.github.adizbek.sper.ui;
+
+import android.view.View;
+import androidx.recyclerview.widget.RecyclerView;
+
+/**ª
+ * @author https://gist.github.com/sheharyarn
+ * https://gist.github.com/sheharyarn/5602930ad84fa64c30a29ab18eb69c6e
+ * Custom implementation of AdapterDataObserver to show empty layouts
+ * for RecyclerView when there's no data
+ * <p>
+ * Usage:
+ * <p>
+ * adapter.registerAdapterDataObserver(new RVEmptyObserver(recyclerView, emptyView));
+ */
+public class RVEmptyObserver extends RecyclerView.AdapterDataObserver {
+    private View emptyView;
+    private RecyclerView recyclerView;
+
+
+    /**
+     * Constructor to set an Empty View for the RV
+     */
+    public RVEmptyObserver(RecyclerView rv, View ev) {
+        this.recyclerView = rv;
+        this.emptyView = ev;
+        checkIfEmpty();
+    }
+
+
+    /**
+     * Check if Layout is empty and show the appropriate view
+     */
+    private void checkIfEmpty() {
+        if (emptyView != null && recyclerView.getAdapter() != null) {
+            boolean emptyViewVisible = recyclerView.getAdapter().getItemCount() == 0;
+            emptyView.setVisibility(emptyViewVisible ? View.VISIBLE : View.GONE);
+            recyclerView.setVisibility(emptyViewVisible ? View.GONE : View.VISIBLE);
+        }
+    }
+
+
+    /**
+     * Abstract method implementations
+     */
+    @Override
+    public void onChanged() {
+        checkIfEmpty();
+    }
+
+    @Override
+    public void onItemRangeInserted(int positionStart, int itemCount) {
+        checkIfEmpty();
+    }
+
+    @Override
+    public void onItemRangeRemoved(int positionStart, int itemCount) {
+        checkIfEmpty();
+    }
+
+}
